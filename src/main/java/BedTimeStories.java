6d070f0542c66ffe4c1a15c926f2ee6af666c66c
bbyhateshit <j.galeta@gmail.com>
2026-04-21 10:54:48 -0400

    Initialized the scanner for user input.

--- src/main/java/BedTimeStories.java
+++ src/main/java/BedTimeStories.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class BedTimeStories {
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the name of a story: ");
+        }
 }
