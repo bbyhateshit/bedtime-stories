c7e07ab731445e0197c889034073cb7ff8bf532a
bbyhateshit <j.galeta@gmail.com>
2026-04-21 11:10:06 -0400

    Full build.

--- src/main/java/BedTimeStories.java
+++ src/main/java/BedTimeStories.java
@@ -1,9 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class BedTimeStories {
-        public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter the name of a story: ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name of a story: ");
+
+        String fileName = scanner.nextLine();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+            String line;
+            int lineNumber = 1;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(lineNumber + ". " + line);
+
+                Thread.sleep(500);
+
+                lineNumber++;
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
         }
+    }
 }
